76d053f0f4047988b8e5bc07669587d18c412763
Volodymyr <roniuk@ukr.net>
2024-05-01 21:13:40 +0300

    commit1

---
+++ src/components/ImageModal/ImageModal.jsx
@@ -0,0 +1,11 @@
+import ReactModal from 'react-modal';
+
+const ImageModal = ({ images }) => {
+  const { urls, alt_description } = images;
+  return (
+    <div>
+      <img src={urls.regular} alt={alt_description} />
+    </div>
+  );
+};
+export default ImageModal;
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,39 @@
+import toast, { Toaster } from "react-hot-toast";
+import css from "./SearchBar.module.css";
+
+const notify = () => toast.error("Please enter text to search for images");
+
+const SearchBar = ({ onSearch }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target.elements.search.value.trim();
+    if (!form) {
+      notify();
+      return;
+    }
+    onSearch(form);
+    e.target.reset();
+  };
+
+  return (
+    <header className={css.header}>
+          <form onSubmit={handleSubmit} className={css.submit}>
+              
+              <div  className={css.search}> 
+                   <button className={css.button} type="submit">
+          🔍
+              </button>
+        <input
+          className={css.search}
+          type="text"
+          name="search"
+          placeholder="Search images and photos"
+                  />
+                  </div>
+        <Toaster position="top-left" />
+      </form>
+    </header>
+  );
+};
+
+export default SearchBar;
